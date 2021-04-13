614290d5bc601cb082d626fb864f3f4081e7e857
bwendel26 <bwendel26999@gmail.com>
2021-04-13 20:15:15 -0300

    Added the icons from react, we made some modifications do iterate on our tasks and present them in the screen

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+// Form
+import { FaPlus } from 'react-icons/fa';
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   
   state = {
     novaTarefa: '',
+    tarefas: [
+      'limpar o quarto',
+      'Estudar Java',
+      'Estudar React',
+      'Fazer o trabalho da faculdade'
+    ]
   }
 
   handleChange = (e) => {
@@ -16,15 +27,30 @@ export default class Main extends Component {
 
   render() {
 
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input 
+            onChange={this.handleChange} 
+            type="text"
+            value={novaTarefa} 
+          />
+          <button type="submit"><FaPlus /></button>
         </form>
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit"/>
+                <FaWindowClose className="delete"/>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }

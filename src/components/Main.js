5d874c7e12ca02abd09d2436b97ea23b640a8d94
bwendel26 <bwendel26999@gmail.com>
2021-04-14 12:11:42 -0300

    We are now saving the state in the localStorage...

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,17 +15,33 @@ export default class Main extends Component {
     index: -1
   }
 
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if(!tarefas) return;
+
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if(tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
-    if(tarefas.indexOf(novaTarefa) != -1) return;
+    if(tarefas.indexOf(novaTarefa) !== -1) return;
 
     const novasTarefas = [...tarefas];
     
-    if(index == -1) {
+    if(index === -1) {
       this.setState({
         tarefas: [...novasTarefas, novaTarefa],
         novaTarefa: '',
